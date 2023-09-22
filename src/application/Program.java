f6611da06251242fc151b90a5bd60f4d8cc7d03c
Roberto Costa <robertocosta66@gmail.com>
2023-09-22 10:11:24 -0300

    Priting possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,22 +14,27 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		ChessMatch chessMactch = new ChessMatch();
+		ChessMatch chessMatch = new ChessMatch();
 		
 		while(true) {
 			UI.clearScreen();
 			try {
-			UI.printBoard(chessMactch.getPieces());
+			UI.printBoard(chessMatch.getPieces());
 			System.out.println();
 			System.out.println();
 			System.out.print("Source: ");
 			ChessPosition source = UI.readChessPosition(sc);	
 			
+			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces(), possibleMoves);
+			
+			
 			System.out.println();
 			System.out.print("Target: ");
 			ChessPosition target = UI.readChessPosition(sc);
 			
-			ChessPiece capturedPiece = chessMactch.performChessMove(source, target);
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
