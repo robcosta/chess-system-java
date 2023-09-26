2065afdfa436c0e5cd46dd276407bbcf15334bbd
Roberto Costa <robertocosta66@gmail.com>
2023-09-26 14:45:34 -0300

    Fix: protected increaseMoveCount decreaseMoveCount

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -18,11 +18,11 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
-	public void increaseMoveCount() {
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
 	
-	public void decreaseMoveCount() {
+	protected void decreaseMoveCount() {
 		moveCount--;
 	}
 	
