eb6f7b119561b1feb461d79a12f572f70114822f
Roberto Costa <robertocosta66@gmail.com>
2023-09-26 14:27:17 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,9 +34,17 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece fopr promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
